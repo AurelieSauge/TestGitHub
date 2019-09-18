763070a98a68edbb530104655fc416f303a95402
Sauge Aurélie (HES) <aurelie.sauge@hes.adhes.hesge.ch>
2019-09-18 11:33:52 +0200

    2eme sauvegarde

--- bonjour.py
+++ bonjour.py
@@ -1,2 +1,8 @@
 print ("Bonjour !")
 print("Bienvenue aux etudiant-e-s 2019 - 2020!")
+
+print("Informatique de Gestion")
+print("1er semestre 2019 - 2020")
+print("Bonjour HEG !")
+
+input("Appuyer sur une toouche pour quitter...")
